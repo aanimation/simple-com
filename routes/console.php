bf6a61ed019e298a4696357fbb3a88e474e1b6d1
arhama <arhamarifuddin@gmail.com>
2025-12-28 13:38:25 +0700

    Add new schedule for DailySalesReportJob

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,11 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\DailySalesReportJob;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new DailySalesReportJob)->dailyAt('23:00');
